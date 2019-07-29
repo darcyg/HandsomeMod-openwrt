8bccb7bb0e79bdead6af3b38a1f70861800b548d
handsomeyingyan <handsomeyingyan@github.com>
2019-07-29 16:52:33 +0800

    add basic files

---
+++ target/linux/sunxi/arm9/target.mk
@@ -0,0 +1,11 @@
+#
+# Copyright (C) 2017 Hauke Mehrtens
+#
+# This is free software, licensed under the GNU General Public License v2.
+# See /LICENSE for more information.
+#
+
+include $(TOPDIR)/rules.mk
+
+BOARDNAME:=Allwinner F1C100s
+CPU_TYPE:=arm926ej-s
